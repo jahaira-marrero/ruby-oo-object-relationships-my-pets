397c02e68db07ba568bca0dda838a7040d7b53da
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-14 16:35:17 -0500

    Updates Cat and Dog and some Associations

--- lib/owner.rb
+++ lib/owner.rb
@@ -23,4 +23,22 @@ class Owner
   def self.reset_all
     @@all = []
   end
+
+  def cats
+    Cat.all.select{|cat| self==cat.owner}
+  end
+
+  def dogs
+    Dog.all.select{|dog| self==dog.owner}
+  end
+
+  def buy_cat(cat_name)
+    Cat.new(cat_name, self)
+  end
+
+  def buy_dog(dog_name)
+    Dog.new(dog_name, self)
+  end
+
+
 end
